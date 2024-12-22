1f735362bd99b8928b69d8063bc72b7e8483dba8
ayozegr05 <ayozegr05@gmail.com>
2024-12-22 14:08:44 +0000

    Añadir la carpeta frontend/BuyABook

---
+++ frontend/BuyABook/components/HelloWave.tsx
@@ -0,0 +1,46 @@
+import { useEffect } from 'react';
+import { StyleSheet, Image } from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  withRepeat,
+  withSequence,
+} from 'react-native-reanimated';
+
+import { ThemedText } from '@/components/ThemedText';
+
+const logoreact = require('../assets/images/logoreact.png'); // Ruta a tu imagen local
+
+
+export function HelloWave() {
+  const rotationAnimation = useSharedValue(0);
+
+  useEffect(() => {
+    rotationAnimation.value = withRepeat(
+      withSequence(withTiming(25, { duration: 150 }), withTiming(0, { duration: 150 })),
+      4 // Run the animation 4 times
+    );
+  }, []);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ rotate: `${rotationAnimation.value}deg` }],
+  }));
+
+  return (
+    <Animated.View style={animatedStyle}>
+      <ThemedText style={styles.text}>👋</ThemedText>
+      <Image source={{uri: 'https://www.penguinlibros.com/pe/2289694-large_default/el-culo.webp'}} style={{ width:200, height: 200 }} />
+      <Image source={logoreact} style={{ width:200, height: 200 }} />
+
+    </Animated.View>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 28,
+    lineHeight: 32,
+    marginTop: -6,
+  },
+});
